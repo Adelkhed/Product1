bdf18a29b289d3d8f110e9c36f6230cd7b3285b6
Adel Khedhiri <khedhiri.adel@gmail.com>
2024-06-18 13:58:38 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CreateProduct from './components/CreateProduct';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Products</h1>  
+        <CreateProduct />
+    </div>
+  );
+}
+
+export default App;
